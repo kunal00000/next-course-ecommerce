dc98f2d8be66f3f6ae0809da8129b26a5c0c35ad
kunal00000 <kunalverma2468@gmail.com>
2023-08-14 04:38:37 +0530

    feat: services

---
+++ src/services/auth.ts
@@ -0,0 +1,45 @@
+import axiosClient from "@/services/axiosClient";
+import { SuccessNotification } from "@/utils/notification";
+
+export const postSignup = async (email: string, password: string) => {
+  const { data } = await axiosClient.post("admin/signup", {
+    username: email,
+    password: password
+  });
+  if (data.message === "Admin created successfully") {
+    console.log("signup success");
+    SuccessNotification(data.message);
+    localStorage.setItem("token", data.token);
+    axiosClient.defaults.headers.common["Authorization"] =
+      "Bearer " + data.token;
+  }
+  return data;
+};
+
+export const postLogin = async (email: string, password: string) => {
+  const { data } = await axiosClient.post(
+    "admin/login",
+    {},
+    {
+      headers: {
+        username: email,
+        password: password
+      }
+    }
+  );
+  if (data.message === "Logged in successfully") {
+    console.log("login success");
+    SuccessNotification(data.message);
+    localStorage.setItem("token", data.token);
+    axiosClient.defaults.headers.common["Authorization"] =
+      "Bearer " + data.token;
+  }
+  return data;
+};
+
+export const getUsername = async () => {
+  const response = await axiosClient.get("admin/username");
+  if (response.status === 200) {
+    return response.data.username as string;
+  }
+};
---
+++ src/services/course.ts
@@ -0,0 +1,23 @@
+import axiosClient from "@/services/axiosClient";
+import { CourseForm } from "@/types/course";
+
+export async function getCourses() {
+  const response = await axiosClient.get("admin/courses");
+  if (response.status === 200) {
+    return response.data;
+  }
+}
+
+export async function postCourse(course: CourseForm) {
+  const response = await axiosClient.post("admin/courses", course);
+  if (response.status === 200) {
+    return response.data;
+  }
+}
+
+export async function updateCourse(id: string, course: CourseForm) {
+  const response = await axiosClient.put(`admin/courses/${id}`, course);
+  if (response.status === 200) {
+    return response.data;
+  }
+}
