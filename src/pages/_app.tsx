81998f07400d0138b94989861523ca5b4a3c1620
kunal00000 <kunalverma2468@gmail.com>
2023-08-14 04:40:55 +0530

    feat: add cover for modals, notifications, and mantine components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+
+import "@/styles/globals.css";
+import { MantineProvider } from "@mantine/core";
+import { ModalsProvider } from "@mantine/modals";
+import { Notifications } from "@mantine/notifications";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider
+      theme={{ fontFamily: "Verdana, sans-serif" }}
+      withGlobalStyles
+      withNormalizeCSS
+    >
+      <ModalsProvider>
+        <Notifications limit={5} />
+        <Component {...pageProps} />
+      </ModalsProvider>
+    </MantineProvider>
+  );
 }
