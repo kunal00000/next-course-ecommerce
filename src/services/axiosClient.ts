61b618103bd795f92f637d74fb4a7e87c6c38541
kunal00000 <kunalverma2468@gmail.com>
2023-08-15 23:20:58 +0530

    feat: axios client

--- src/services/axiosClient.ts
+++ src/services/axiosClient.ts
@@ -4,11 +4,8 @@ const baseUrl = process.env.NEXT_API_BASE_URL || "http://localhost:3000";
 const axiosClient: AxiosInstance = axios.create({
   baseURL: baseUrl,
   headers: {
-    "Content-Type": "application/json"
-  }
+    "Content-Type": "application/json",
+  },
 });
-
-axiosClient.defaults.headers.common["Authorization"] =
-  "Bearer " + localStorage.getItem("token") || "";
 
 export default axiosClient;
