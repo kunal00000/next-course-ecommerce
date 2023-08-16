4c3d8b30a42001aa3eb94cc8fd1ccc0e3db47d28
kunal00000 <kunalverma2468@gmail.com>
2023-08-16 23:09:37 +0530

    feat: signup page

---
+++ src/pages/signup.tsx
@@ -0,0 +1,76 @@
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+import { Button, Card, Group, Input, PasswordInput, Text } from "@mantine/core";
+
+import { IconAt, IconLock } from "@tabler/icons-react";
+
+import { postSignup } from "@/services/auth";
+import { ErrorNotification } from "@/utils/notification";
+
+function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  function handleEmail(e: any) {
+    setEmail(e.target.value);
+  }
+  function handlePassword(e: any) {
+    setPassword(e.target.value);
+  }
+
+  async function onSignup() {
+    try {
+      const signupResponse = await postSignup(email, password);
+      if (signupResponse.message === "Admin created successfully") {
+        router.push("/dashboard");
+      }
+    } catch (err: any) {
+      ErrorNotification(err.response.data.message);
+    }
+  }
+
+  return (
+    <Card
+      withBorder
+      shadow="md"
+      radius="md"
+      className="xs:w-[90vw] md:w-[30vw] mx-auto my-[20vh]"
+    >
+      <Card.Section withBorder inheritPadding py={4} my={20}>
+        <Text size={20} fw={700} color="teal">
+          Register to the website
+        </Text>
+      </Card.Section>
+      <Input.Wrapper withAsterisk label="Username" onChange={handleEmail}>
+        <Input id="input-email" icon={<IconAt />} placeholder="Your username" />
+      </Input.Wrapper>
+      <br />
+      <PasswordInput
+        id="input-password"
+        placeholder="Password..."
+        label="Password"
+        icon={<IconLock size="1rem" />}
+        withAsterisk
+        onChange={handlePassword}
+        className=""
+      />
+      <Group position="center" className="my-4 mt-7">
+        <Button onClick={onSignup} variant="outline" color="teal">
+          Sign up
+        </Button>
+      </Group>
+      <Group>
+        <Text size={"sm"} className="ml-auto">
+          Already a user?
+          <Button variant="light" compact onClick={() => router.push("/login")}>
+            Login
+          </Button>
+        </Text>
+      </Group>
+    </Card>
+  );
+}
+
+export default Register;
--- src/services/axiosClient.ts
+++ src/services/axiosClient.ts
@@ -1,11 +1,11 @@
 import axios, { AxiosInstance } from "axios";
 
-const baseUrl = process.env.NEXT_API_BASE_URL || "http://localhost:3000";
+const baseUrl = "http://localhost:3000";
 const axiosClient: AxiosInstance = axios.create({
   baseURL: baseUrl,
   headers: {
-    "Content-Type": "application/json",
-  },
+    "Content-Type": "application/json"
+  }
 });
 
 export default axiosClient;
