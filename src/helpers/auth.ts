48a7f06893c60df43c95ff7b199f14325662baf2
kunal00000 <kunalverma2468@gmail.com>
2023-08-17 00:10:11 +0530

    feat: login route complete

--- src/helpers/auth.ts
+++ src/helpers/auth.ts
@@ -10,38 +10,35 @@ export const postSignup = async (email: string, password: string) => {
   if (data.success === true) {
     console.log("signup success");
     SuccessNotification(data.message);
-    return data;
   } else {
     console.log("signup error");
     ErrorNotification(data.message);
     console.log(data);
   }
+  return data;
 };
 
-// export const postLogin = async (email: string, password: string) => {
-//   const { data } = await axiosClient.post(
-//     "admin/login",
-//     {},
-//     {
-//       headers: {
-//         username: email,
-//         password: password
-//       }
-//     }
-//   );
-//   if (data.message === "Logged in successfully") {
-//     console.log("login success");
-//     SuccessNotification(data.message);
-//     localStorage.setItem("token", data.token);
-//     axiosClient.defaults.headers.common["Authorization"] =
-//       "Bearer " + data.token;
-//   }
-//   return data;
-// };
+export const postLogin = async (email: string, password: string) => {
+  const { data } = await axiosClient.post(
+    "/api/admin/login",
+    {},
+    {
+      headers: {
+        username: email,
+        password: password
+      }
+    }
+  );
+  if (data.success === true) {
+    console.log("login success");
+    SuccessNotification(data.message);
+  }
+  return data;
+};
 
-// export const getUsername = async () => {
-//   const response = await axiosClient.get("admin/username");
-//   if (response.status === 200) {
-//     return response.data.username as string;
-//   }
-// };
+export const getUsername = async () => {
+  const response = await axiosClient.get("admin/username");
+  if (response.status === 200) {
+    return response.data.username as string;
+  }
+};
---
+++ src/pages/login.tsx
@@ -0,0 +1,81 @@
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+import { Button, Card, Group, Input, PasswordInput, Text } from "@mantine/core";
+
+import { IconAt, IconLock } from "@tabler/icons-react";
+
+import { postLogin } from "@/helpers/auth";
+import { ErrorNotification } from "@/utils/notification";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  function handleEmail(e: any) {
+    setEmail(e.target.value);
+  }
+  function handlePassword(e: any) {
+    setPassword(e.target.value);
+  }
+
+  async function onLogin() {
+    try {
+      const loginResponse = await postLogin(email, password);
+      if (loginResponse.message === "Logged in successfully") {
+        router.push("/dashboard");
+      }
+    } catch (err: any) {
+      console.log(err);
+      ErrorNotification(err.response.data.message);
+    }
+  }
+
+  return (
+    <Card
+      withBorder
+      shadow="md"
+      radius="md"
+      className="xs:w-[90vw] md:w-[30vw] mx-auto my-[20vh]"
+    >
+      <Card.Section withBorder inheritPadding py={4} my={20}>
+        <Text size={20} fw={700} color="teal">
+          Login to admin dashboard
+        </Text>
+      </Card.Section>
+      <Input.Wrapper withAsterisk label="Username" onChange={handleEmail}>
+        <Input id="input-email" icon={<IconAt />} placeholder="Your username" />
+      </Input.Wrapper>
+      <br />
+      <PasswordInput
+        id="input-password"
+        placeholder="Password..."
+        label="Password"
+        icon={<IconLock size="1rem" />}
+        withAsterisk
+        onChange={handlePassword}
+        className=""
+      />
+      <Group position="center" className="my-4 mt-7">
+        <Button onClick={onLogin} variant="outline" color="teal">
+          Login
+        </Button>
+      </Group>
+      <Group>
+        <Text size={"sm"} className="ml-auto">
+          New here?{" "}
+          <Button
+            variant="light"
+            compact
+            onClick={() => router.push("/signup")}
+          >
+            Register
+          </Button>
+        </Text>
+      </Group>
+    </Card>
+  );
+}
+
+export default Login;
