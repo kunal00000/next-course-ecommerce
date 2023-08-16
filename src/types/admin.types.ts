09a499d2a62441e4b98b7dae05400bb0e0ac368f
kunal00000 <kunalverma2468@gmail.com>
2023-08-16 22:55:41 +0530

    refactor: types and zod schemas

--- src/types/admin.types.ts
+++ src/types/admin.types.ts
@@ -1,11 +1,12 @@
+import mongoose from "mongoose";
 import z from "zod";
 
 export const admin_zod_schema = z
   .object({
     _id: z.string().uuid().optional(),
-    username: z.string().min(1).max(20).uuid(),
+    username: z.string().min(1).max(20),
     password: z.string().min(1).max(40),
-    myCourses: z.array(z.string())
+    myCourses: z.array(z.instanceof(mongoose.Types.ObjectId))
   })
   .strict();
 
