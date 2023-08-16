221bd06ba0a591bdbaa96ef386eb35c26fc2517c
kunal00000 <kunalverma2468@gmail.com>
2023-08-16 22:55:23 +0530

    feat: admin models

--- src/models/admin.model.ts
+++ src/models/admin.model.ts
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const admin_db_schema = new mongoose.Schema({
   username: String,
   password: String,
-  myCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course" }],
+  myCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course" }]
 });
 
-export const AdminModel = mongoose.model("admin", admin_db_schema);
+export const AdminModel =
+  mongoose.models.Admin || mongoose.model("Admin", admin_db_schema);
